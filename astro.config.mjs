09c6203913482b6a0d4095191418e218e87a8fa4
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2025-07-26 10:47:43 -0500

    fix: downgrade astro to version 4.4.5 and remove netlify dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://portfolio-david-uribe.netlify.app/',
   output: 'server',  // Configuración SSR
-  adapter: netlify(),
-  experimental: {
-    session: true,  // Habilitar las sesiones experimentales
-  },
 });
