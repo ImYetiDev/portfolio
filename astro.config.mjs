29f1f0876462e18a5f458bf19c6351782d5a295e
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2025-07-26 10:24:07 -0500

    chore: update dependencies in package.json
    - Added @astrojs/netlify version 6.5.3
    - Updated astro from version 4.4.5 to ^5.3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
+import netlify from '@astrojs/netlify';
 
-import robotsTxt from "astro-robots-txt"
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://portfolio-david-uribe.netlify.app/',
-  output: 'server',
-})
+  output: 'server',  // Configuración SSR
+  adapter: netlify(),
+  experimental: {
+    session: true,  // Habilitar las sesiones experimentales
+  },
+});
