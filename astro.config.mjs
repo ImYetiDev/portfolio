d8f7852e9a57cff51b0938ab86648ca9a80835eb
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2025-07-26 10:56:01 -0500

    chore: update dependencies for improved compatibility and features
    - Replace @astrojs/tailwind with version 6.0.2
    - Add @astrojs/netlify dependency at version 6.5.3
    - Upgrade astro to version 5.3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://portfolio-david-uribe.netlify.app/',
-  output: 'server',  // Configuración SSR
+  output: 'server',
+  adapter: netlify(),
+  experimental: {
+    session: true,  // Habilita la configuración experimental de sesiones
+  },
 });
